804547963a9f45beddcc4a751a8269f7005c1337
Marchabar <marchabar1@alum.us.es>
2023-12-24 14:21:13 +0100

    refactor:  code distribution

--- footy/settings.py
+++ footy/settings.py
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'footy.apps.equipos',
-    'footy.apps.jugadores',
-    'footy.apps.partidos',
-    'footy.apps.jornadas',
 ]
 
 MIDDLEWARE = [
