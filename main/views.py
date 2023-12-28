da381bc0ecb43a5785a04800dcc00eafc5f9a2a3
Marchabar <marchabar1@alum.us.es>
2023-12-28 20:13:54 +0000

    feat: filter working but not 100%

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from main.models import Equipo, Jornada, Partido, Jugador
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser
+from whoosh.query import Or
+
 
 
 def index(request):
@@ -19,5 +23,25 @@ def listado_partidos_jornada(request, id_jornada):
     return render(request, 'listado-partidos-jornadas.html', {'jornada':jornada,'partidos': partidos})
 
 def listado_jugadores(request):
-    jugadores = Jugador.objects.all()
-    return render(request, 'listado-jugadores.html', {'jugadores': jugadores})
+    query = request.GET.get('query', '')
+
+    if query:
+        directorio = 'Index'  
+        ix = open_dir(directorio)
+
+        with ix.searcher() as searcher:
+            nombre_query = QueryParser("nombre", ix.schema).parse(query)
+            edad_query = QueryParser("edad", ix.schema).parse(query)
+            posicion_query = QueryParser("posicion", ix.schema).parse(query)
+            nacionalidad_query = QueryParser("nacionalidad_nombre", ix.schema).parse(query)
+            equipo_query = QueryParser("equipo", ix.schema).parse(query)
+
+            combined_query = Or([nombre_query, edad_query, posicion_query, nacionalidad_query, equipo_query])
+
+            jugadores = searcher.search(combined_query)
+
+            jugador_objects = [Jugador.objects.get(pk=int(jugador['id_jugador'])) for jugador in jugadores]
+    else:
+        jugador_objects = Jugador.objects.all()
+
+    return render(request, 'listado-jugadores.html', {'jugadores': jugador_objects, 'query': query})
